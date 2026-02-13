c1ea1b73ad52c10c34888964eac590521a65ab34
Brylliant Dev <153876117+brylliant-dev@users.noreply.github.com>
2026-02-13 16:24:46 +0800

    Change paragraph to header for preferences title

--- silktide-consent-manager.js
+++ silktide-consent-manager.js
@@ -422,7 +422,7 @@ class SilktideCookieBanner {
 
     const modalContent = `
       <header>
-        <p>${preferencesTitle}</p>                    
+        <h3>${preferencesTitle}</h3>                    
         ${closeModalButton}
       </header>
       ${preferencesDescription}
