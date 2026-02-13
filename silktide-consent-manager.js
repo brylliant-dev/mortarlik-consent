5fcc0766e620beb62d01725e4a0c203eee37414c
Brylliant Dev <153876117+brylliant-dev@users.noreply.github.com>
2026-02-13 16:22:10 +0800

    Change header from h3 to p for preferences title
    Update modal header to use paragraph instead of heading

--- silktide-consent-manager.js
+++ silktide-consent-manager.js
@@ -422,7 +422,7 @@ class SilktideCookieBanner {
 
     const modalContent = `
       <header>
-        <h3>${preferencesTitle}</h3>                    
+        <p>${preferencesTitle}</p>                    
         ${closeModalButton}
       </header>
       ${preferencesDescription}
